afc60fd392c1f09979c542a4165904781c5320ee
androstravelsandtours-svg <androstravelsandtours@gmail.com>
2026-06-03 17:39:27 +0500

    Add server health check

--- server.js
+++ server.js
@@ -13,6 +13,12 @@ const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
   createServer((req, res) => {
+    if (req.url === '/__health') {
+      res.writeHead(200, { 'content-type': 'text/plain' });
+      res.end('MAS Marine real server.js is running');
+      return;
+    }
+
     handle(req, res);
   }).listen(port, hostname, () => {
     console.log(`MAS Marine Admin ready on http://${hostname}:${port}`);
